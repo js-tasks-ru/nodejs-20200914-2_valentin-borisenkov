5075ae42786e4ea2414629e092992bb14571b1f9
valentin <valentyn.borysenkov@oracle.com>
2020-10-01 03:32:02 +0300

    add safe path check back

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -10,7 +10,6 @@ const trySendFileContent = (req, res, filepath) => {
 
   // QUESTION : req.on('aborted') has never worked here, maybe 'cause I'm using node 12 yet?
   // - При обрыве соединения необходимо завершить работу стрима.
-  //
   req.on('close', () => {
     if (req.aborted) {
       fileStream.destroy();
@@ -22,7 +21,7 @@ const trySendFileContent = (req, res, filepath) => {
   // helper console log to identify stream is closed on connection abort
   // (i.e. slow network simulation in Chrome and then stop the page loading)
   fileStream.on('close', () => {
-    // console.log('stream closed');
+    console.log('stream closed');
   });
 
   res.statusCode = 200;
@@ -33,16 +32,32 @@ const trySendFileContent = (req, res, filepath) => {
   }).pipe(res);
 };
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-  // console.log(pathname);
+const isSafePath = (pathname, rootDir) => {
+  let isErrorDecodingPath = false;
 
-  if (pathname.split('/').length !== 1) {
-    res.statusCode = 400;
-    res.end(http.STATUS_CODES[400]);
+  try {
+    pathnameDecoded = decodeURIComponent(pathname);
+  } catch (e) {
+    console.log(e);
+    isErrorDecodingPath = true;
   }
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  if (isErrorDecodingPath || pathname.indexOf('\0') !==-1 || pathname === rootDir) return false;
+
+  return pathname.indexOf(rootDir) === 0;
+};
+
+server.on('request', (req, res) => {
+  const pathname = path.normalize(url.parse(req.url).pathname.slice(1));
+  const rootDir = path.join(__dirname, 'files');
+  const filepath = path.normalize(path.join(rootDir, pathname));
+
+  if (!isSafePath(filepath, rootDir) || pathname.split('/').length !== 1) {
+    res.statusCode = 400;
+    res.end(http.STATUS_CODES[400]);
+    return;
+  }
+
 
   switch (req.method) {
     case 'GET':
